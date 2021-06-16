3bf0700a2c45b7766205bd68f7ab4f38274104af
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2021-06-16 15:23:25 -0700

    some of step 5

--- bank-of-codepath-ui/src/components/AddTransaction/AddTransaction.js
+++ bank-of-codepath-ui/src/components/AddTransaction/AddTransaction.js
@@ -1,7 +1,8 @@
+import axios from "axios";
 import { useState } from "react"
 import "./AddTransaction.css"
 
-export default function AddTransaction() {
+export default function AddTransaction(props) {
 
   const [isProcessing, setIsProcessing] = useState(false);
   const [error, setError] = useState(null);
@@ -11,9 +12,25 @@ export default function AddTransaction() {
     amount: ""
   })
 
-  const handleOnFormChange = () => {
-    
+  const handleOnFormChange = (evt) => {
+    setForm(oldForm => ({
+      ...oldForm, 
+      [evt.target.name]: evt.target.value
+    }))
   } 
+
+  const handleOnSubmit = async () => {
+
+    const newTransaction = await axios.post("/bank/transactions", {
+      description: form.description,
+      category: form.category,
+      amount: form.amount
+    })
+
+    setIsProcessing(true);
+    await props.AddTransaction(newTransaction);
+    setIsProcessing(false);
+  }
 
   return (
     <div className="AddTransaction">
@@ -23,18 +40,18 @@ export default function AddTransaction() {
         <div className="fields">
           <div className="field">
             <label>Description</label>
-            <input type="text" name="description" placeholder="Enter a description..." onChange={handleOnFormChange} />
+            <input type="text" name="description" placeholder="Enter a description..." value={form.description} onChange={handleOnFormChange} />
           </div>
           <div className="field">
             <label>Category</label>
-            <input type="text" name="category" placeholder="Enter a category..." onChange={handleOnFormChange} />
+            <input type="text" name="category" placeholder="Enter a category..." value={form.category} onChange={handleOnFormChange} />
           </div>
           <div className="field" style={{ flex: 0.5 }}>
             <label>Amount (cents)</label>
-            <input type="number" name="amount" onChange={handleOnFormChange} />
+            <input type="number" name="amount" value={form.amount} onChange={handleOnFormChange} />
           </div>
 
-          <button className="btn add-transaction" type="submit">
+          <button className="btn add-transaction" type="submit" onClick={handleOnSubmit}>
             Add
           </button>
         </div>
